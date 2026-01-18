4cf598082f5bb6a3171074420dd33ab4cdbdbf7e
MAC <makinderidwan73@gmail.com>
2026-01-19 00:59:30 +0100

    added cors

--- src/core/express/index.ts
+++ src/core/express/index.ts
@@ -7,12 +7,35 @@ import routes from "../../routes";
 import errorHandler from "../../middlewares/errorHandler";
 import swaggerUi from "swagger-ui-express";
 import { specs } from "../../docs/swagger";
+import cors from "cors";
 
 dotenv.config();
 
 connectDB();
 
 const app = express();
+
+const allowedOrigins = [
+  "http://localhost:6500",          
+  "http://localhost:6501",         
+  "https://your-frontend.render.com"
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl)
+    if (!origin) return callback(null, true);
+    
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true, // This MUST be true for the jwt cookie to work
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 app.use(express.json());
 
