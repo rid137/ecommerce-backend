d5e38438017a91cdf788a5461bb68c28e1006c33
MAC <makinderidwan73@gmail.com>
2026-04-01 10:03:43 +0100

    added callbackUrl to validation

--- src/validators/payment.validator.ts
+++ src/validators/payment.validator.ts
@@ -2,7 +2,8 @@ import Joi from "joi";
 
 export const initializePaymentSchema = Joi.object({
   orderId: Joi.string().required().length(24),
-  email: Joi.string().email().required()
+  email: Joi.string().email().required(),
+  callbackUrl: Joi.string().uri().required()
 });
 
 export const verifyPaymentSchema = Joi.object({
