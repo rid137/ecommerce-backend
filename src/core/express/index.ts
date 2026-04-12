63bbc0b51ddbd5eabb2d21ff3925bf1cbb04e80d
MAC <makinderidwan73@gmail.com>
2026-04-12 22:57:28 +0100

    added new FE origin

--- src/core/express/index.ts
+++ src/core/express/index.ts
@@ -18,7 +18,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:6500",          
   "http://localhost:6501",         
-  "https://your-frontend.render.com"
+  "https://velora-e-commerce.pages.dev"
 ];
 
 const corsOptions = {
